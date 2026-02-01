30382ffe571db62a2cb795a66beffdafabc13721
Jonathan Sage <sagejonathan.tesol@gmail.com>
2026-02-01 08:55:54 -0700

    add suspense in survey load due to search params

--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,5 +1,17 @@
+import { Suspense } from "react";
+
 import Survey from "@/components/forms/survey";
 
 export default function SurveyPage() {
-  return <Survey />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex min-h-[50vh] items-center justify-center bg-[#FCF9ED]">
+          <p className="text-neutral-500">Loading…</p>
+        </div>
+      }
+    >
+      <Survey />
+    </Suspense>
+  );
 }
