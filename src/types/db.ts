1d1043b0c3254446589db614fbac3f05b13caebf
Jonathan Sage <sagejonathan.tesol@gmail.com>
2026-02-01 08:37:35 -0700

    add db types for forms used in server actions

---
+++ src/types/db.ts
@@ -0,0 +1,20 @@
+
+export type WaitlistInsert = {
+  id: number;
+  name: string;
+  email: string;
+  activities?: string[];
+  availability: string;
+  how_did_you_hear?: string;
+};
+
+export type SurveyInsert = {
+  waitlist_id?: string;
+  networking_pain_selected?: string[];
+  networking_pain?: string | null;
+  career_dev_pain: string | null;
+  struggle_selected?: string[];
+  struggle_other: string | null;
+  feature_selected?: string[];
+  feature_other?: string | null;
+};
